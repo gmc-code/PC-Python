466a915544df2627fbabbcefd0136fb44e523700
gmc-code <gmccarthy@tpg.com.au>
2022-12-25 09:22:12 +1100

    up

--- docs/turtle/files/grid_first_quad_with_house.py
+++ docs/turtle/files/grid_first_quad_with_house.py
@@ -68,6 +68,23 @@ def label_axes(t, width, height, spacing, tcolor):
         t.write(i + ytranslate, align="center", font=("Arial", 12, "normal"))
 
 
+def manual_labels(t, start_pos, tcolor):
+    t.color(tcolor, tcolor)
+    t.pensize(1)
+    x0 = start_pos[0]
+    y = start_pos[1]
+    # x axis
+    for i in range(1, 11):
+        t.pu()
+        t.goto(x0 + i * 50, y)
+        t.seth(-90)
+        t.pd()
+        t.write(str(i) + "/10" , align="center", font=("Arial", 12, "normal"))
+        t.pu()
+        t.goto(x0 + i * 50, y)
+        t.pd()
+        t.fd(20)
+        t.pu()
 
 
 s = turtle.Screen()
@@ -83,19 +100,17 @@ draw_axes(grid_turtle, 800, 600, 4, "red")
 label_axes(grid_turtle, 800, 600, 100, "red")
 grid_turtle.ht()
 
-ytranslate = -200
-xtranslate = -300
-"""drawing code below"""
 
+"""drawing code below"""
 import houses as h
 
 s.tracer(0, 0)
-
 t = turtle.Turtle()
 t.speed(0)
-
+ytranslate = -200
+xtranslate = -300
 h.windowed_house(t, length=500, height=300, start_pos=(0 + xtranslate, 0 + ytranslate), w_sides="LR")
-
+manual_labels(grid_turtle, (0 + xtranslate, 250 + ytranslate), "blue")
     
 t.ht()
 
