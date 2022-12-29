8790f3e7293f4b1345945b938b8922811fd12c4b
gmc-code <gmccarthy@tpg.com.au>
2022-12-30 10:37:50 +1100

    up

--- docs/turtle/files/turtle_test copy.py
+++ docs/turtle/files/turtle_test copy.py
@@ -40,6 +40,34 @@ def house_roof(t, length, height, start_pos):
     sh.isosceles(t, base=r_length, height=r_height, start_pos=r_pos, penw=1, penc="black", fillc="brown")
 
 
+def house_window4(t, length, height, start_pos, w_side, fillc="light blue"):
+    """draw a 4 pane square window.
+        each window pane is half the total length of the window.
+        right hand window; 1/5 of house length; 2/3 of house length from left of house up 1/3 of height of house
+        left hand window; 1/5 of house length; 1/20 of house length from left of house up 1/3 of height of house
+
+    Args:
+        t (class turtle.Turtle): turtle instance.
+        length (int, optional): length of house.
+        height (int, optional): height of house.
+        start_pos (tuple, optional): bottom left of house.
+        w_sides (str): L for left side of house; R for right and LR for both.
+        fillc (str, optional): window pane colour. Defaults to "light blue".
+    """
+    w_length = length//10
+    if "L" in w_side:
+        w_pos = (start_pos[0] + length//20, start_pos[1] + height//3)
+    elif "R" in w_side:
+        w_pos = (start_pos[0] + 2*length//3, start_pos[1] + height//3)
+    else:
+        w_pos = (start_pos[0] + length//20, start_pos[1] + height//3)
+
+    sh.square(t, length=w_length, start_pos=w_pos, fillc="light blue")
+    sh.square(t, length=w_length, start_pos=(w_pos[0] + w_length, w_pos[1]), fillc="light blue")
+    sh.square(t, length=w_length, start_pos=(w_pos[0], w_pos[1] + w_length), fillc="light blue")
+    sh.square(t, length=w_length, start_pos=(w_pos[0] + w_length, w_pos[1] + w_length), fillc="light blue")
+
+
 def house(t, length=60, height=40, start_pos=(0, 0), w_sides=None):
     """draw a house with 0-2 windows
 
@@ -58,8 +86,16 @@ def house(t, length=60, height=40, start_pos=(0, 0), w_sides=None):
 
     # roof
     house_roof(t, length, height, start_pos)
-    
+
     # windows
+    if w_sides is not None:
+        if "R" in w_sides:
+            # right hand window
+            house_window4(t, length, height, start_pos, "R")
+        if "L" in w_sides:
+            # left hand window
+            house_window4(t, length, height, start_pos, "L")
+
 
 house(t, length=600, height=300, start_pos=(-300, -200), w_sides="LR")
 
